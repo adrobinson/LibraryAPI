6e39ba01cd1d7dc76106f0c576bdae795c6ebfde
Adam Robinson <adrobinsonuk25@gmail.com>
2025-05-28 14:24:53 +0100

    Setup classses to configure review endpoints

---
+++ Library/src/main/java/com/example/Library/controller/ReviewController.java
@@ -0,0 +1,9 @@
+package com.example.Library.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class ReviewController {
+}
